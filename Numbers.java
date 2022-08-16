eb3aa2fba6799182586b3c468bdc82177099a6d8
PTsishuk <107580202+PTsishuk@users.noreply.github.com>
2022-08-16 14:42:29 -0400

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -2,7 +2,7 @@ public class Numbers {
 
 	public static void main(String[] args) {
 	
-		int[] arr = {1,2,3,4,5,6,7,8,9,10};
+		int[] arr = new int[20];
 		
 		for (int i=1; i<=arr.length; i++) {
 			System.out.println(i);
